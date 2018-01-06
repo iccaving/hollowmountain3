155289436eaceaa78236f85a41b00323053d08e7
jdhare <jack.d.hare@gmail.com>
2018-01-06 14:39:29 +0000

    all upper case all lowercase

--- images/conv.py
+++ images/conv.py
@@ -1,4 +1,5 @@
 import os
+from subprocess import call
 
 def count(text):
 	count = 0
@@ -9,10 +10,8 @@ def count(text):
 
 for dirpath, dirs, files in os.walk(os.getcwd()):
 	for file in files:
-		num = count(file)
-		if num > 1:
-			os.rename(os.path.join(dirpath, file), os.path.join(dirpath, file.replace('.','_',num - 1 )))
-
-for dirpath, dirs, files in os.walk(os.getcwd()):
-	for file in files:
-		os.rename(os.path.join(dirpath, file), os.path.join(dirpath, file.replace(')','_').replace('(','_').replace("'",'_')))
+		name, ext = os.path.splitext(file)
+		call(['git','mv',os.path.join(dirpath, file),os.path.join(dirpath, name + ext.lower())])
+		print(ext)
+		if ext == ".jpg" or ext == ".png":
+			os.rename(os.path.join(dirpath, file), os.path.join(dirpath, name + ext.upper()))
